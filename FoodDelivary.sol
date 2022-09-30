5f652f4336fa9e012d77f02a90c07a0d6ea084f2
hemanth bhushan <hemanthbhushan6@gmail.com>
2022-09-30 11:50:01 +0530

    commit message

--- FoodDelivary.sol
+++ FoodDelivary.sol
@@ -2,6 +2,7 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
@@ -39,7 +40,6 @@ constructor()ERC1155(""){
  //here the nft id are stored for example when the new hotel gets registered the Nft of Id  1 is deposited to the user
  //simiarly to the user who orders gets the thanku nft which has the nft id of 2 and etc   
  struct NftIds{
-    string purpose;
     uint nftId;
     uint256 totalSupply;
  }  
@@ -48,13 +48,12 @@ constructor()ERC1155(""){
     NftIds[] public getThePurposeOfNftIds;
     Hotel[] registeredHotels;
     
-    mapping (uint256 => NftIds) public idToNfts;
+    mapping (string => NftIds) public idToNfts;
     mapping  (address=>mapping(address => bool)) public whitelisteManager;
     mapping (address=>mapping(string=>uint)) public hotelId;
 
     //when hotel gets registered the manager of the hotel get the Nft as the proof use in the future for the hotel registration the nftId will be 1
 
-   mapping (string=>uint) public NftId;
 
   
 
@@ -64,6 +63,7 @@ constructor()ERC1155(""){
 
         require(whitelisteManager[owner()][msg.sender] == true,"hotelManger need to get approval from the owner");
         require(hotelDeposit != msg.sender && hotelDeposit != address(0),"check the rules" );
+        string memory _purpose = "Hotel";
 
         hotelCount.increment();
         uint Id = hotelCount.current();
@@ -71,7 +71,8 @@ constructor()ERC1155(""){
 
        
 
-        _mint(msg.sender,NftId["Hotel"],1,"");
+        _mint(msg.sender,idToNfts[_purpose].nftId,1,"");
+        idToNfts[_purpose].totalSupply++;
 
         registeredHotels.push(registered);
         hotelId[msg.sender][hotelName] = Id; 
@@ -79,9 +80,10 @@ constructor()ERC1155(""){
     }
 
       function changeHotelManager(address _newManager,string memory _hotelName) external returns(address ){
+          string memory _purpose = "Hotel";
           require(whitelisteManager[owner()][msg.sender] == true,"hotelManger need to get approval from the owner");
           require(_newManager != address(0),"cant the make the zero address as the manager");
-          require( balanceOf(msg.sender, NftId["Hotel"])>0,"the hotel is missing the hotel registration nft ");
+          require( balanceOf(msg.sender, idToNfts[_purpose].nftId)>0,"the hotel is missing the hotel registration nft ");
           uint256 _Id = hotelId[msg.sender][_hotelName];
               
                uint256 count;
@@ -89,7 +91,7 @@ constructor()ERC1155(""){
                for(uint256 i=0;i<length;i++){
                    if(_Id== registeredHotels[i].hotelId){
                        //internal function erc1155
-                       _safeTransferFrom(msg.sender, _newManager,NftId["Hotel"] ,1,"");
+                       _safeTransferFrom(msg.sender, _newManager,idToNfts[_purpose].nftId ,1,"");
                        whitelisteManager[owner()][msg.sender] = false;
                        registeredHotels[i].hotelManager = _newManager;
                        hotelId[_newManager][_hotelName] = _Id;
@@ -104,6 +106,10 @@ constructor()ERC1155(""){
                //new manager should get the approval from the owner
  } 
 
+ function removeHotel() external  onlyOwner{
+     
+ }
+
 
 
     function hotelManagerGetApproveFromOwner(address hotelManager,bool check) external  onlyOwner returns(bool){
@@ -115,25 +121,18 @@ constructor()ERC1155(""){
     function checkRegisteredHotels() public view returns(Hotel[] memory){
         return  registeredHotels;
     }
-    function setNftIds(string memory typeOfRegisstration,uint256 _NftId) external  onlyOwner{
-    NftId[typeOfRegisstration] = _NftId;
 
-}
 
     function addNewNftType(string memory _purpose, uint256 _totalSupply) external onlyOwner returns(uint256 newNftId ){
         nftIdCount.increment();
         uint _idNo = nftIdCount.current();
+    
 
-          idToNfts[_idNo] = NftIds({
-              purpose:_purpose,
+          idToNfts[_purpose] = NftIds({
               nftId:_idNo,
               totalSupply:_totalSupply
           });
-        // newNftId = totalMinted.length-1; 
-        // NftIds memory nftIds = NftIds(_purpose,newNftId);
-        // totalSupplyOfNFT.push(_totalSupply);
-        // totalMinted.push(0);
-        getThePurposeOfNftIds.push(idToNfts[_idNo]);
+        getThePurposeOfNftIds.push(idToNfts[_purpose]);
 
     }
 
